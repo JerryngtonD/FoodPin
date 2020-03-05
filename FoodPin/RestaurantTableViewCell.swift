9806525b512f932d7367b2ede20819d18bfdbd8f
DAVYDOV Evgeny <Evgeny.DAVYDOV@raiffeisen.ru>
2020-03-05 14:49:21 +0300

    uiTableViewController restraunt added with custom cell

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by DAVYDOV Evgeny on 05.03.2020.
+//  Copyright © 2020 DAVYDOV Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var locationLabel: UILabel!
+  @IBOutlet var typeLabel: UILabel!
+  @IBOutlet var thumbnailImageView: UIImageView!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
