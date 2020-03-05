4bc7738afcb4427d362c13b5dc6733e3161938b0
DAVYDOV Evgeny <Evgeny.DAVYDOV@raiffeisen.ru>
2020-03-05 12:09:53 +0300

    Initial Commit

---
+++ FoodPin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodPin
+//
+//  Created by DAVYDOV Evgeny on 05.03.2020.
+//  Copyright © 2020 DAVYDOV Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
