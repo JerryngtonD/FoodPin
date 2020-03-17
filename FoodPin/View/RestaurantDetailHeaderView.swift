d6be4dac0ca7186deae182bb82bd35152fede32c
jerryngton <jerryngtond@gmail.com>
2020-03-17 18:29:17 +0300

    added rubic font

---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Evgeny on 17.03.2020.
+//  Copyright © 2020 DAVYDOV Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: NSObject {
+
+}
