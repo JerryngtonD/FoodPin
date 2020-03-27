b40be8f8ff1bbaed95615c72768538016b95cdfb
jerryngton <jerryngtond@gmail.com>
2020-03-27 23:01:10 +0300

    added custom pun annotation

--- FoodPin/Controller/MapViewController.swift
+++ FoodPin/Controller/MapViewController.swift
@@ -9,14 +9,35 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     
     var restaurant = Restaurant()
 
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+        if annotation.isKind(of: MKUserLocation.self) { return nil }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        if annotationView == nil {
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        annotationView?.glyphText = "😋"
+        annotationView?.markerTintColor = UIColor.orange
+        return annotationView
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mapView.delegate = self
+        
+        // view setting
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        
         // Convert address to coordinate and annotate it on map
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
