91f58bcc907890085cd48ac983cb1718e51735ec
jerryngton <jerryngtond@gmail.com>
2020-03-28 12:51:34 +0300

    added scale up and fade-in animation colaboration

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -34,36 +34,47 @@ class ReviewViewController: UIViewController {
         
         backgroundImageView.addSubview(blurEffectView)
         
+        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
+        let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        
         // Make the button invisible
         for rateButton in rateButtons {
+            rateButton.transform = moveScaleTransform
             rateButton.alpha = 0
         }
+    
     }
     
     override func viewWillAppear(_ animated: Bool) {
             UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations:
             {
                 self.rateButtons[0].alpha = 1.0
+                self.rateButtons[0].transform = .identity
             }, completion: nil)
 
             UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations:
               {
                   self.rateButtons[1].alpha = 1.0
+                  self.rateButtons[1].transform = .identity
               }, completion: nil)
 
             UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations:
                {
                    self.rateButtons[2].alpha = 1.0
+                   self.rateButtons[2].transform = .identity
                }, completion: nil)
 
             UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations:
                 {
                     self.rateButtons[3].alpha = 1.0
+                    self.rateButtons[3].transform = .identity
                 }, completion: nil)
 
             UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations:
                 {
                     self.rateButtons[4].alpha = 1.0
+                    self.rateButtons[4].transform = .identity
                 }, completion: nil)
     }
    
