0a3fc13d3e4fc9dc4e3e35dce03f00d3308d901f
jerryngton <jerryngtond@gmail.com>
2020-03-29 10:20:40 +0300

    deleted duplicate code + added animation when appearing close

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -19,6 +19,8 @@ class ReviewViewController: UIViewController {
     //    rateButtons[4] // Angry button
     @IBOutlet var rateButtons: [UIButton]!
     
+    @IBOutlet var closeButton: UIButton!
+    
     var restaurant = Restaurant()
     
     
@@ -37,45 +39,57 @@ class ReviewViewController: UIViewController {
         let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
         let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
         let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        let moveUpTransform = CGAffineTransform.init(translationX: 0, y: -600)
         
         // Make the button invisible
         for rateButton in rateButtons {
             rateButton.transform = moveScaleTransform
             rateButton.alpha = 0
         }
-    
+        
+        closeButton.transform = moveUpTransform
     }
     
     override func viewWillAppear(_ animated: Bool) {
-            UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations:
-            {
+        for index in 0...4 {
+            UIView.animate(withDuration: 0.4, delay: (0.1 + 0.05 * Double(index)), options: [], animations: {
+                self.rateButtons[index].alpha = 1.0
+                self.rateButtons[index].transform = .identity
+            }, completion: nil)
+        }
+        
+        // Animate the close button
+        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+           self.closeButton.transform = .identity
+        }, completion: nil)
+            
+            /*
+            UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
                 self.rateButtons[0].alpha = 1.0
                 self.rateButtons[0].transform = .identity
             }, completion: nil)
 
-            UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations:
-              {
-                  self.rateButtons[1].alpha = 1.0
-                  self.rateButtons[1].transform = .identity
-              }, completion: nil)
+            UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+                self.rateButtons[1].alpha = 1.0
+                self.rateButtons[1].transform = .identity
+            }, completion: nil)
 
-            UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations:
-               {
-                   self.rateButtons[2].alpha = 1.0
-                   self.rateButtons[2].transform = .identity
-               }, completion: nil)
+            UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+                self.rateButtons[2].alpha = 1.0
+                self.rateButtons[2].transform = .identity
+            }, completion: nil)
 
-            UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations:
-                {
-                    self.rateButtons[3].alpha = 1.0
-                    self.rateButtons[3].transform = .identity
-                }, completion: nil)
+            UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+                self.rateButtons[3].alpha = 1.0
+                self.rateButtons[3].transform = .identity
+            }, completion: nil)
 
-            UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations:
-                {
-                    self.rateButtons[4].alpha = 1.0
-                    self.rateButtons[4].transform = .identity
-                }, completion: nil)
+            UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+                self.rateButtons[4].alpha = 1.0
+                self.rateButtons[4].transform = .identity
+            }, completion: nil)
+            */
+            
     }
    
     
