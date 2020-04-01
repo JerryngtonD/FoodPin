aa8e7b85da1ff35b36c2e7e9d067bd993dd3c189
jerryngton <jerryngtond@gmail.com>
2020-04-02 02:41:33 +0300

    added image picker for adding and taking photo

--- FoodPin/Controller/NewRestaurantController.swift
+++ FoodPin/Controller/NewRestaurantController.swift
@@ -55,6 +55,42 @@ class NewRestaurantController: UITableViewController, UITextFieldDelegate {
     }
 
     override func viewDidLoad() {
-          super.viewDidLoad()
+        super.viewDidLoad()
+        
+         // Configure navigation bar appearance
+        if let customFont = UIFont(name: "Rubik-Medium", size: 35.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60),
+                NSAttributedString.Key.font: customFont ]
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            let photoSourceRequestController = UIAlertController(title: "", message: "Choose your photo source", preferredStyle: .actionSheet)
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .camera
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            
+            let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default, handler: { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .photoLibrary
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            
+            photoSourceRequestController.addAction(cameraAction)
+            photoSourceRequestController.addAction(photoLibraryAction)
+            
+            present(photoSourceRequestController, animated: true, completion: nil)
+        }
     }
 }
