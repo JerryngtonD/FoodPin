9452842cff19f3cae30d14f714a496f595ea11a5
jerryngton <jerryngtond@gmail.com>
2020-04-10 12:29:32 +0300

    added update status page controller on user swipe

--- FoodPin/Controller/WalkthroughViewController.swift
+++ FoodPin/Controller/WalkthroughViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WalkthroughViewController: UIViewController {
+class WalkthroughViewController: UIViewController,  WalkthroughPageViewControllerDelegate {
     @IBOutlet var pageControl: UIPageControl!
     
     @IBOutlet var nextButton: UIButton! {
@@ -19,6 +19,8 @@ class WalkthroughViewController: UIViewController {
     }
     
     @IBOutlet var skipButton: UIButton!
+    
+    var walkthroughPageViewController: WalkthroughPageViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +28,45 @@ class WalkthroughViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func skipButtonTapped(sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination
+        if let pageViewController = destination as? WalkthroughPageViewController {
+            walkthroughPageViewController = pageViewController
+            walkthroughPageViewController?.walkthroughDelegate = self
+        }
+    }
+    
+    @IBAction func nextButtonTapped(sender: UIButton) {
+        if let index = walkthroughPageViewController?.currentIndex {
+            switch index {
+            case 0...1: walkthroughPageViewController?.forwardPage()
+            case 2: dismiss(animated: true, completion: nil)
+            default: break
+            }
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let index = walkthroughPageViewController?.currentIndex {
+            switch index {
+            case 0...1:
+                nextButton.setTitle("NEXT", for: .normal)
+                skipButton.isHidden = false
+            case 2:
+                nextButton.setTitle("GET STARTED", for: .normal)
+                skipButton.isHidden = true
+            default: break
+            }
+            pageControl.currentPage = index
+        }
+    }
+    
+    func didUpdatePageIndex(currentIndex: Int) {
+        updateUI()
+    }
 }
