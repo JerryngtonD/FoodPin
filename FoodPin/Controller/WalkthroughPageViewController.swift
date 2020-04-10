8b6929129c81dc15bed9df272b07c4eb5a29cf3e
jerryngton <jerryngtond@gmail.com>
2020-04-10 11:55:10 +0300

    Walkthrough files

---
+++ FoodPin/Controller/WalkthroughPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WalkthroughPageViewController.swift
+//  FoodPin
+//
+//  Created by Evgeny on 10.04.2020.
+//  Copyright © 2020 DAVYDOV Evgeny. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: NSObject {
+
+}
